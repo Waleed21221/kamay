7b1ec29443edfe53002925883069a38334520720
AyeshaAmeendys <sheikhzadi70@gmail.com>
2020-03-16 16:12:50 +0500

    aisha change 2

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -341,31 +341,10 @@
                             </ul>
                         </li>
 
-                        <li class="nav-main-item{{ request()->is('examples/*') ? ' open' : '' }}">
-                            <a class="nav-main-link nav-main-link-submenu" data-toggle="submenu" aria-haspopup="true" aria-expanded="true" href="#">
-                                <i class="nav-main-link-icon si si-bulb"></i>
-                                <span class="nav-main-link-name">testing</span>
-                            </a>
-                            <ul class="nav-main-submenu">
-                                <li class="nav-main-item">
-                                    <a class="nav-main-link{{ request()->is('examples/plugin-helper') ? ' active' : '' }}" href="/examples/plugin-helper">
-                                        <span class="nav-main-link-name">Add Worker</span>
-                                    </a>
-                                </li>
-                                <li class="nav-main-item">
-                                    <a class="nav-main-link{{ request()->is('examples/plugin-init') ? ' active' : '' }}" href="/examples/plugin-init">
-                                        <span class="nav-main-link-name">List Worker</span>
-                                    </a>
-                                </li>
+                        
                             </ul>
                         </li>
-                        <li class="nav-main-heading">More</li>
-                        <li class="nav-main-item">
-                            <a class="nav-main-link" href="/">
-                                <i class="nav-main-link-icon si si-globe"></i>
-                                <span class="nav-main-link-name">Landing</span>
-                            </a>
-                        </li>
+                       
                     </ul>
                 </div>
                 <!-- END Side Navigation -->
